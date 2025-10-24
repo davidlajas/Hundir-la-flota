13c9dc8f295087b846f47a48ba38c8762174d6ab
davidlajas <davidlajas@gmail.com>
2025-10-24 08:21:46 +0200

    comentarios en funciones

--- funciones.py
+++ funciones.py
@@ -3,20 +3,20 @@ import random
 
 #Creación de tablero
 def tablero(tamaño):
-    return np.full((tamaño,tamaño),"#")
+    return np.full((tamaño,tamaño),"#") #Hacemos un tablero de tamaño por tamaño
 
 
 #Creación de flota
 def crear_barco(eslora, tablero):
-    eslora_base = 0
-    hori_vert = np.random.randint(0,1)
-    fila = np.random.randint(0,10-eslora)
-    columna = np.random.randint(0,10-eslora)
+    eslora_base = 0 #definimos eslora base para ir igualando
+    hori_vert = np.random.randint(0,1) #con esta variable se decide si va vertical u horizontalmente
+    fila = np.random.randint(0,10-eslora) # se decide una fila al azar - la eslora para que no se salga del tabero
+    columna = np.random.randint(0,10-eslora) # se decide una columna al azar - la eslora para que no se salga del tabero
     tablero[fila,columna] = "O"
-    while eslora_base != eslora:
+    while eslora_base != eslora: #con este bucle seguirá añadiendo eslora hasta que se iguale al que hemos indicado en la lista
         if hori_vert == 0: #horizontal
             tablero[fila+eslora_base,columna] = "O"
-            eslora_base = eslora_base +1
+            eslora_base = eslora_base +1 #cada vez que se ejecuta el bucle se añade uno al contador de la eslora base
             
         else: #vertical
             tablero[fila,columna+eslora_base] = "O"
